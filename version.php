f773210d63cf380d0db3f57a30c26d97a9a68eaa
John Rivera Gonzalez <johnriveragonzalez7@gmail.com>
2026-09-01 13:44:25 -0400

    Bump to 1.0.3 and refine chart-image overflow

--- version.php
+++ version.php
@@ -17,7 +17,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_completionreport';
-$plugin->version   = 2026090102;
+$plugin->version   = 2026090103;
 $plugin->requires  = 2024100700; // Moodle 4.5+ (subsections / Moodle 5 compatible).
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.0.2';
+$plugin->release   = '1.0.3';
