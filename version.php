3f373dcc0ad3086689fa4548fa01d22f2bf5853a
John Rivera Gonzalez <johnriveragonzalez7@gmail.com>
2026-09-01 13:38:53 -0400

    Bump version to 1.0.1

--- version.php
+++ version.php
@@ -17,7 +17,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_completionreport';
-$plugin->version   = 2026090100;
+$plugin->version   = 2026090101;
 $plugin->requires  = 2024100700; // Moodle 4.5+ (subsections / Moodle 5 compatible).
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.0.0';
+$plugin->release   = '1.0.1';
